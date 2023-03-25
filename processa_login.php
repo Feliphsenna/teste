9ae203df949f6ad2c1922dacb45860b5d0198fd1
Feliphsenna <128866963+Feliphsenna@users.noreply.github.com>
2023-03-25 10:22:17 -0300

    Update processa_login.php

--- processa_login.php
+++ processa_login.php
@@ -1,12 +1,21 @@
 <?php
-  $usuario = $_POST["usuario"];
-  $senha = $_POST["senha"];
-
-  if($usuario == "Admin" && $senha == "1234"){
-    echo "<script>alert('Login efetuado com sucesso!');</script>";
-    echo "<script>window.location.href='pagina_restrita.php';</script>";
-  }else{
-    echo "<script>alert('Usuário ou senha incorretos.');</script>";
-    echo "<script>window.location.href='index.php';</script>";
-  }
+    // Verifica se o formulário foi submetido
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        
+        // Verifica se as informações do formulário são válidas
+        if($_POST["username"] == "Admin" && $_POST["password"] == "1234") {
+            
+            // Redireciona para o Google
+            header("Location: https://www.google.com");
+            
+        } else {
+            
+            // Redireciona para a página inicial
+            header("Location: index.html");
+            
+        }
+        
+        exit();
+        
+    }
 ?>
